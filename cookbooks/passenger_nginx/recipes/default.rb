2164de490b23901f955d9264c1ece024c394e61b
Alex Clemesha <clemesha@gmail.com>
2011-12-27 21:46:50 -0800

    New 'passenger_nginx' cookbook

---
+++ cookbooks/passenger_nginx/recipes/default.rb
@@ -0,0 +1,23 @@
+bash "install_passenger_nginx" do
+  code <<-EOH
+    sudo apt-add-repository ppa:brightbox/passenger-nginx
+    sudo apt-get update
+    sudo apt-get -y install nginx-full
+    sudo gem install passenger
+    sudo passenger-status
+  EOH
+end
+
+template "/etc/nginx/conf.d/passenger.conf" do
+  source "passenger.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+template "/etc/nginx/sites-enabled/default" do
+  source "sites-enabled-default.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
